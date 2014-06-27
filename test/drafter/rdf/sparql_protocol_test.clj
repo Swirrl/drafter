4c7a52cefd2679c35e8de1bd2f5102131ab6bd86
Rick Moynihan <rick.m@swirrl.com>
2014-06-27 16:56:44 +0100

    Forgot to commit tests.

---
+++ test/drafter/rdf/sparql_protocol_test.clj
@@ -0,0 +1,40 @@
+(ns drafter.rdf.sparql-protocol-test
+  (:require
+   [grafter.rdf.protocols :as pr]
+   [grafter.rdf :refer :all]
+   [grafter.rdf.sesame :refer :all]
+   [drafter.rdf.sparql-protocol :refer :all]
+   [clojure.java.io :as io]
+   [drafter.rdf.sesame-test :refer [test-triples]]
+   [clojure.data.json :as json]
+   [clojure.test :refer :all])
+  (:import [java.io ByteArrayOutputStream]))
+
+;; as all these tests are query only we can initialise the test db
+;; once.
+
+(def db (let [db (repo (memory-store))]
+          (pr/add db "http://foo.com/" test-triples)
+          db))
+
+(deftest sparql-results!-test
+  (testing "Streams sparql results into output stream"
+    (let [baos (ByteArrayOutputStream.)]
+
+      (-> (prepare-query db "SELECT * WHERE { ?s ?p ?o }")
+          (sparql-results! baos "application/sparql-results+json"))
+
+      (let [output (-> baos .toByteArray String. json/read-str)]
+        (is (map? output))))))
+
+(deftest sparql-end-point-test
+
+  (let [end-point (sparql-end-point "/live/sparql" db)]
+
+    (testing "Standard SPARQL query"
+      (is (= true (end-point {:request-method :get
+                              :uri "/live/sparql"
+                              :params {:query "SELECT * WHERE { ?s ?p ?o } LIMIT 10"}
+                              :headers {"Accept" "text/csv"}})))))
+
+  )
