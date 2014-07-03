3d0a35b728c9f8335448dd92a0f63f1b9f44d0a8
Rick Moynihan <rick.m@swirrl.com>
2014-07-03 16:23:32 +0100

    Remove old jena work (it's been moved to the experimental-jena-work branch).

--- project.clj
+++ project.clj
@@ -7,10 +7,6 @@
                                     :passphrase :env}]]
 
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.apache.jena/jena-core "2.11.2"]
-                 [org.apache.jena/jena-arq "2.11.2"]
-                 [org.apache.jena/jena-tdb "1.0.2"]
-                 [org.apache.jena/jena-iri "1.0.2"]
                  [me.raynes/fs "1.4.4"] ; ;filesystem utils
                  [lib-noir "0.8.4"]
                  [ring-server "0.3.1"]
