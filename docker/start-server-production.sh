e96afb9dbdb3866796579422c87cbbe57181ce2c
RicSwirrl <ric@swirrl.com>
2015-01-08 15:14:01 +0000

    change dockerfile to use the drafter jar passed in

--- docker/start-server-production.sh
+++ docker/start-server-production.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /drafter
-java -jar -Djava.awt.headless=true -Ddrafter.repo.path=/data/drafter-database /drafter/target/drafter-0.1.0-SNAPSHOT-standalone.jar
+java -jar -Djava.awt.headless=true -Ddrafter.repo.path=/data/drafter-database /drafter/target/drafter.jar
