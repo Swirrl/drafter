a33051c8f7ce2416a29acba0bd7d3bb4f76a4c27
Rick Moynihan <rick.m@swirrl.com>
2015-01-28 11:19:03 +0000

    Bump version to 0.2.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject drafter "0.1.0"
+(defproject drafter "0.2.0-SNAPSHOT"
   :description "Backend PMD service"
   :url "http://github.com/Swirrl/drafter"
   :license {:name "Proprietary & Commercially Licensed Only"
