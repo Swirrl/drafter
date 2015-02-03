98b78b37eb30a930e692c2c85ab94ad3a15e42a9
RicSwirrl <ric@swirrl.com>
2015-02-03 00:46:23 +0000

    more prod cof tweaks

--- docker/start-server-production.sh
+++ docker/start-server-production.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /drafter
-java -Xmx8g -jar -Djava.awt.headless=true -Ddrafter.repo.path=/data/drafter-database /drafter/target/drafter.jar
+java -Xms2048m -Xmx4096m -XX:PermSize=512m -XX:MaxPermSize=512m -jar -Djava.awt.headless=true -Ddrafter.repo.path=/data/drafter-database /drafter/target/drafter.jar
