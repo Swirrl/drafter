2f0939e170d1be81c0dafd2d8d9e3633e7abbb62
Robin Gower <robin.git@infonomics.ltd.uk>
2015-02-10 17:37:00 +0000

    Increase grafter version to 0.3.1

--- project.clj
+++ project.clj
@@ -17,7 +17,7 @@
                  [wrap-verbs "0.1.1"]
                  [selmer "0.6.9"]
 
-                 [grafter "0.3.0" :exclusions [[org.openrdf.sesame/sesame-runtime]]]
+                 [grafter "0.3.1" :exclusions [[org.openrdf.sesame/sesame-runtime]]]
 
                  [org.openrdf.sesame/sesame-queryrender "2.7.14"]
                  [org.openrdf.sesame/sesame-runtime "2.7.14"]
