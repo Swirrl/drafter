c08cf60855c938e40ac2bd7f3ce1d30703f22b16
RicSwirrl <ric@swirrl.com>
2015-02-13 12:08:09 +0000

    tweaked the timeout settings

--- src/drafter/rdf/sparql_protocol.clj
+++ src/drafter/rdf/sparql_protocol.clj
@@ -227,11 +227,12 @@
                                                                                 pquery response-mime-type publish))
           _ (connect-operation query-operation write-fn)
 
-          ;hard-code query timeouts for now - 30s for each result and 2 minutes for the entire operation
-          query-operation-timeouts (create-timeouts 30000 120000)]
+          ;hard-code query timeouts for now - 60s for each result and 4 minutes for the entire operation
+          ;TODO: make this configurable!
+          query-operation-timeouts (create-timeouts 60000 24000)]
 
       (submit-operation query-operation query-operation-timeouts)
-      
+
       {:status 200
        :headers {"Content-Type" (get-sparql-response-content-type response-mime-type)}
        ;; Designed to work with piped-input-stream this fn will be run
