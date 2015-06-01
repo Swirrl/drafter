8f21a255325b94293fcd8970306daa9658eb7650
Scott Lowe <scottwithtwotees@gmail.com>
2015-06-01 17:37:14 +0100

    FIX for issue #29 "Specifying no draft graphs with union-live off reveals all graphs including state graph"

--- src/drafter/common/sparql_routes.clj
+++ src/drafter/common/sparql_routes.clj
@@ -2,37 +2,31 @@
   (:require [clojure.set :as set]
             [drafter.rdf.draft-management :as mgmt :refer [live-graphs]]))
 
-(defn- maybe-merge-with-live [repo union-with-live graphs graphs-with-drafts]
-  (let [live-graphs (when union-with-live
-                      (live-graphs repo))
-        supplied-graphs (if (instance? String graphs)
-                          #{graphs}
-                          graphs)]
-
-
-    ;; remove the graphs-with-drafts from the live-graphs
-    (set/union
-      (set/difference live-graphs graphs-with-drafts)
-      supplied-graphs)))
-
 (defn supplied-drafts
   "Parses out the set of \"graph\"s supplied on the request.
 
-If no graphs are found in the request, it returns the set of live
+  If no graphs are found in the request, it returns the set of live
   graphs.
 
-This implementation does not enforce any security restrictions, and
+  This implementation does not enforce any security restrictions, and
   assumes that the client is trustworthy."
   [repo {:keys [params] :as request}]
 
   (let [graphs (get params :graph)
-        union-with-live (get params :union-with-live false)
+        union-with-live? (get params :union-with-live false)
         draft-set (if (instance? String graphs)
                     #{graphs}
                     graphs)
         ;; get the graphs with drafts from graph-map
-        graphs-with-drafts (into #{} (map str (keys (mgmt/graph-map repo draft-set))))]
-
-    (maybe-merge-with-live repo union-with-live graphs graphs-with-drafts)
-    ;;(live-graphs repo)
-    ))
+        graphs-with-drafts (into #{}
+                                 (map str
+                                      (keys (mgmt/graph-map repo draft-set))))
+        live-graphs (if union-with-live?
+                      (live-graphs repo)
+                      #{})
+        supplied-graphs (if (instance? String graphs)
+                          #{graphs}
+                          graphs)]
+    (set/union
+      (set/difference live-graphs graphs-with-drafts)
+      supplied-graphs)))
