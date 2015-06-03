e631711761fa70048d50a7733d3305899ecbb783
RicSwirrl <ric@swirrl.com>
2015-06-03 22:52:26 +0100

    dockerstuff

--- docker/start-server-production.sh
+++ docker/start-server-production.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 cd /drafter
-java -Xms2048m -Xmx${MAX_HEAP:-4096}m -XX:PermSize=512m -XX:MaxPermSize=512m -jar -Djava.awt.headless=true -Ddrafter.repo.path=/data/drafter-database /drafter/target/drafter.jar
+
+#java -Xms2048m -Xmx${MAX_HEAP:-4096}m -XX:PermSize=512m -XX:MaxPermSize=512m -jar -Djava.awt.headless=true -Ddrafter.repo.path=/data/drafter-database /drafter/target/drafter.jar
+
+#expects a stardog to be linked.
+env SPARQL_UPDATE_ENDPOINT=http://$STARDOG_PORT_5820_TCP_ADDR:5820/pmd-data/update \
+  SPARQL_QUERY_ENDPOINT=http://$STARDOG_PORT_5820_TCP_ADDR:5820/pmd-data/query \
+  java -Xms2048m -Xmx${MAX_HEAP:-4096}m -XX:PermSize=512m -XX:MaxPermSize=512m -jar \
+  -Djava.awt.headless=true \
+  /drafter/target/drafter.jar
--- docker/start-server-stardog-production.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd /drafter
-
-#expects a stardog to be linked.
-env SPARQL_UPDATE_ENDPOINT=http://$STARDOG_PORT_5820_TCP_ADDR:5820/pmd-data/update \
-  SPARQL_QUERY_ENDPOINT=http://$STARDOG_PORT_5820_TCP_ADDR:5820/pmd-data/query \
-  java -Xms2048m -Xmx${MAX_HEAP:-4096}m -XX:PermSize=512m -XX:MaxPermSize=512m -jar \
-  -Djava.awt.headless=true \
-  /drafter/target/drafter.jar
