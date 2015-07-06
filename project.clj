4d59d0b1a389673d13d7e505198770c628fa2d46
Rick Moynihan <rick.m@swirrl.com>
2015-07-06 14:12:14 +0100

    Build with JENA 3.0.0-SNAPSHOT from our private lein repo.
    We compiled the JENA build ourselves with the following command:
    
    mvn -DcreateChecksum=true -Dmaven.test.skip=true install
    
    NOTE: The JENA build failed, but it fails on artifacts not relevant to
    drafter, so we just copied the artifacts which built without error into
    our repository.

--- project.clj
+++ project.clj
@@ -4,11 +4,12 @@
   :license {:name "Proprietary & Commercially Licensed Only"
             :url "http://swirrl.com/"}
 
-  ;; SNAPSHOT repository for JENA builds
-  :repositories [["jena-snapshots" {:url "https://repository.apache.org/content/repositories/snapshots/"
-                                    :releases false}]]
-
   :dependencies [
+                 ;; NOTE jena 3.0.0-SNAPSHOT was compiled from source with maven
+                 ;; and pushed into our private repo at commit:
+                 ;; d58c1a1abc7dfb2a58ce5b8c04e176940fecbb9a
+                 ;;
+                 ;; This is necessary for the new rewriting stuff.
                  [org.apache.jena/jena-arq "3.0.0-SNAPSHOT" :exclusions [org.slf4j/slf4j-api
                                                                          com.fasterxml.jackson.core/jackson-core
                                                                          org.slf4j/jcl-over-slf4j
