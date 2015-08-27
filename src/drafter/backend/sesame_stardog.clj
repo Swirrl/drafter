8a58c9dc3b8a5d3d345e335829b1c0b76bae7bf9
lkitching <lee@swirrl.com>
2015-08-27 11:12:26 +0100

    Move migrate live queries into backend.sesame-stardog.
    Move the queries only used by the Stardog migrate-live process into
    the backend.sesame-stardog namespace.

--- src/drafter/backend/sesame_stardog.clj
+++ src/drafter/backend/sesame_stardog.clj
@@ -94,15 +94,90 @@
 (def ^:private execute-update-fn
   (create-execute-update-fn get-repo (fn [conn pquery] (repo/evaluate pquery))))
 
+(defn- move-like-tbl-wants-super-slow-on-stardog-though
+  "Move's how TBL intended.  Issues a SPARQL MOVE query.
+  Note this is super slow on stardog 3.1."
+  [source destination]
+  ;; Move's how TBL intended...
+  (str "MOVE SILENT <" source "> TO <" destination ">"))
+
+(defn- delete-insert-move
+  "Move source graph to destination.  Semantically the same as MOVE but done
+  with DELETE/INSERT's.
+
+  Massively quicker  on stardog than a MOVE."
+  [source destination]
+  (str
+   ;; first clear the destination, then...
+   "DELETE {"
+   "  GRAPH <" destination "> {?s ?p ?o}"
+   "} WHERE {"
+   "  GRAPH <" destination "> {?s ?p ?o} "
+   "};"
+   ;; copy the source to the destination, and...
+   "INSERT {"
+   "  GRAPH <" destination "> {?s ?p ?o}"
+   "} WHERE { "
+   "  GRAPH <" source "> {?s ?p ?o}"
+   "};"
+   ;; remove the source (draft) graph
+   "DELETE {"
+   "  GRAPH <" source "> {?s ?p ?o}"
+   "} WHERE {"
+   " GRAPH <" source "> {?s ?p ?o}"
+   "}"))
+
+(defn graph-non-empty-query [graph-uri]
+  (str
+   "ASK WHERE {
+    SELECT * WHERE {
+      GRAPH <" graph-uri "> { ?s ?p ?o }
+    } LIMIT 1
+  }"))
+
+(defn graph-non-empty?
+  "Returns true if the graph contains any statements."
+  [repo graph-uri]
+  (repo/query repo (graph-non-empty-query graph-uri)))
+
+(defn graph-empty?
+  "Returns true if there are no statements in the associated graph."
+  [repo graph-uri]
+  (not (graph-non-empty? repo graph-uri)))
+
+(defn should-delete-live-graph-from-state-after-draft-migrate?
+  "When migrating a draft graph to live, the associated 'is managed
+  graph' statement should be removed from the state if graph if:
+  1. The migrate operation is a delete (i.e. the draft graph is empty)
+  2. The migrated graph is the only draft associated with the live
+  graph."
+  [repo draft-graph-uri live-graph-uri]
+  (and
+   (graph-empty? repo draft-graph-uri)
+   (not (mgmt/has-more-than-one-draft? repo live-graph-uri))))
+
+;;Repository -> String -> { queries: [String], live-graph-uri: String }
+(defn- migrate-live-queries [db draft-graph-uri]
+  (if-let [live-graph-uri (mgmt/lookup-live-graph db draft-graph-uri)]
+    (let [move-query (delete-insert-move draft-graph-uri live-graph-uri)
+          delete-state-query (mgmt/delete-draft-state-query draft-graph-uri)
+          live-public-query (mgmt/set-isPublic-query live-graph-uri true)
+          queries [move-query delete-state-query live-public-query]
+          queries (if (should-delete-live-graph-from-state-after-draft-migrate? db draft-graph-uri live-graph-uri)
+                    (conj queries (mgmt/delete-live-graph-from-state-query live-graph-uri))
+                    queries)]
+      {:queries queries
+       :live-graph-uri live-graph-uri})))
+
 (defn- migrate-graphs-to-live-impl! [backend graphs]
   "Migrates a collection of draft graphs to live through a single
   compound SPARQL update statement. This overrides the default sesame
   implementation which uses a transaction to coordinate the
   updates. Explicit UPDATE statements do not take part in transactions
   on the remote sesame SPARQL client."
-  (log/info "Starting make-live for graph" graphs)
+  (log/info "Starting make-live for graphs " graphs)
   (let [repo (get-repo backend)
-        graph-migrate-queries (mapcat #(:queries (mgmt/migrate-live-queries repo %)) graphs)
+        graph-migrate-queries (mapcat #(:queries (migrate-live-queries repo %)) graphs)
         update-str (util/make-compound-sparql-query graph-migrate-queries)]
     (update! repo update-str))
   (log/info "Make-live for graph(s) " graphs " done"))
--- src/drafter/rdf/draft_management.clj
+++ src/drafter/rdf/draft_management.clj
@@ -236,7 +236,7 @@
   (if (draft-exists? db draft-graph-uri)
     (lookup-live-graph db draft-graph-uri)))
 
-(defn- delete-live-graph-from-state-query [live-graph-uri]
+(defn delete-live-graph-from-state-query [live-graph-uri]
   (str "DELETE WHERE"
        "{"
        (with-state-graph
@@ -335,86 +335,4 @@
 (defn migrate-live! [backend graph]
   (migrate-graphs-to-live! backend [graph]))
 
-(defn- move-like-tbl-wants-super-slow-on-stardog-though
-  "Move's how TBL intended.  Issues a SPARQL MOVE query.
-  Note this is super slow on stardog 3.1."
-  [source destination]
-  ;; Move's how TBL intended...
-  (str "MOVE SILENT <" source "> TO <" destination ">"))
 
-(defn- delete-insert-move
-  "Move source graph to destination.  Semantically the same as MOVE but done
-  with DELETE/INSERT's.
-
-  Massively quicker  on stardog than a MOVE."
-  [source destination]
-  (str
-   ;; first clear the destination, then...
-   "DELETE {"
-   "  GRAPH <" destination "> {?s ?p ?o}"
-   "} WHERE {"
-   "  GRAPH <" destination "> {?s ?p ?o} "
-   "};"
-   ;; copy the source to the destination, and...
-   "INSERT {"
-   "  GRAPH <" destination "> {?s ?p ?o}"
-   "} WHERE { "
-   "  GRAPH <" source "> {?s ?p ?o}"
-   "};"
-   ;; remove the source (draft) graph
-   "DELETE {"
-   "  GRAPH <" source "> {?s ?p ?o}"
-   "} WHERE {"
-   " GRAPH <" source "> {?s ?p ?o}"
-   "}"))
-
-(defn graph-non-empty-query [graph-uri]
-  (str
-   "ASK WHERE {
-    SELECT * WHERE {
-      GRAPH <" graph-uri "> { ?s ?p ?o }
-    } LIMIT 1
-  }"))
-
-(defn graph-non-empty?
-  "Returns true if the graph contains any statements."
-  [repo graph-uri]
-  (query repo (graph-non-empty-query graph-uri)))
-
-(defn graph-empty?
-  "Returns true if there are no statements in the associated graph."
-  [repo graph-uri]
-  (not (graph-non-empty? repo graph-uri)))
-
-(defn should-delete-live-graph-from-state-after-draft-migrate?
-  "When migrating a draft graph to live, the associated 'is managed
-  graph' statement should be removed from the state if graph if:
-  1. The migrate operation is a delete (i.e. the draft graph is empty)
-  2. The migrated graph is the only draft associated with the live
-  graph."
-  [repo draft-graph-uri live-graph-uri]
-  (and
-   (graph-empty? repo draft-graph-uri)
-   (not (has-more-than-one-draft? repo live-graph-uri))))
-
-;;migrate-live-queries :: Repository -> String -> { queries: [String], live-graph-uri: String }
-(defn migrate-live-queries [db draft-graph-uri]
-  (if-let [live-graph-uri (lookup-live-graph db draft-graph-uri)]
-    (let [move-query (delete-insert-move draft-graph-uri live-graph-uri)
-          delete-state-query (delete-draft-state-query draft-graph-uri)
-          live-public-query (set-isPublic-query live-graph-uri true)
-          queries [move-query delete-state-query live-public-query]
-          queries (if (should-delete-live-graph-from-state-after-draft-migrate? db draft-graph-uri live-graph-uri)
-                    (conj queries (delete-live-graph-from-state-query live-graph-uri))
-                    queries)]
-      {:queries queries
-       :live-graph-uri live-graph-uri})))
-
-(defn migrate-live!
-  "Moves the triples from the draft graph to the draft graphs live destination."
-  [db draft-graph-uri]
-  (log/debug (str "Migrating graph: " draft-graph-uri " to live"))
-  (let [{:keys [queries live-graph-uri]} (migrate-live-queries db draft-graph-uri)
-        query-str (util/make-compound-sparql-query queries)]
-    (update! db query-str)
-    (log/info (str "Migrated graph: " draft-graph-uri " to live graph " live-graph-uri))))
