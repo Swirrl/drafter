17cb8566260dfe3bea8d5f4cee955187b661a18d
Rick Moynihan <rick.m@swirrl.com>
2016-02-02 17:44:52 +0000

    Update to use grafter 0.7.0
    This grafter works with sesame 2.8.9 and allows the creation of Literals
    with a nil language

--- project.clj
+++ project.clj
@@ -38,7 +38,7 @@
 
                  [swirrl/lib-swirrl-server "0.3.0" :exclusions [clout org.clojure/java.classpath]]
 
-                 [grafter "0.6.1"]
+                 [grafter "0.7.0"]
                  [org.openrdf.sesame/sesame-queryresultio-sparqlxml "2.8.9"]
 
                  [grafter/vocabularies "0.1.3"]
