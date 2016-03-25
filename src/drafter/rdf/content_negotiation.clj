1f63752a9c2ae2f5c834acdf7356101691ac8d7b
lkitching <lee@swirrl.com>
2016-03-25 10:59:39 +0000

    Add content-negotiation module.
    Content negotiation is currently split between sparql-protocol which
    defines which mime types are supported and their preference levels,
    and the backends which choose how to write query results in the
    negotiated format. Backends can also fail the content negotiation
    if they do not support the mime type negotiated in sparql-protocol.
    
    This approach is error-prone and requires backends to keep their
    mime type mappings in sync with those supported in sparql-protocol.
    The mapping defined in sparql-protocol also duplicates the information
    already defined within sesame, which contains query result format
    types which specify a list of supported mime types. The mapping is
    incomplete which means drafter does not support all mappings supported
    by sesame writers.
    
    Add an rdf.content-negotiation module based around the sesame result
    format types. It exports a negotiate function which returns a result
    format object given the query type (ASK, CONSTRUCT or SELECT) and
    the client's Accept request header. If negotiation fails, nil is
    returned. If the negotation succeeds, backends will be responsible
    for writing the query results in the given format.

---
+++ src/drafter/rdf/content_negotiation.clj
@@ -0,0 +1,126 @@
+(ns drafter.rdf.content-negotiation
+  (:require [drafter.rdf.rewriting.result-rewriting :refer [result-handler-wrapper]]
+            [clojure.java.io :as io]
+            [grafter.rdf.formats :as formats]
+            [ring.middleware.accept :refer [wrap-accept]])
+  (:import [java.nio.charset Charset]
+           [java.io Writer OutputStream]
+           [org.openrdf.query.impl MapBindingSet]
+           [org.openrdf.rio RDFFormat RDFWriter RDFWriterFactory RDFWriterRegistry]
+           [org.openrdf.query.resultio BooleanQueryResultFormat TupleQueryResultFormat]
+           [org.openrdf.query.resultio.text.csv SPARQLResultsCSVWriter]
+           [org.openrdf.query.resultio.text.tsv SPARQLResultsTSVWriter]))
+
+(def ^:private ascii (Charset/forName "US-ASCII"))
+(def csv-rdf-format (RDFFormat. "CSV" "text/csv" ascii "csv" false true))
+(def tsv-rdf-format (RDFFormat. "TSV" "text/tab-separated-values" nil "tsv" false true))
+
+(defn- query-result-writer->rdf-writer [rdf-format result-writer]
+  (reify RDFWriter
+    (getRDFFormat [this] rdf-format)
+    (getWriterConfig [this] (.getWriterConfig result-writer))
+    (setWriterConfig [this config] (.setWriterConfig result-writer config))
+    (getSupportedSettings [this] (.getSupportedSettings result-writer))
+    (startRDF [this]
+      (.startQueryResult result-writer '("s" "p" "o")))
+    (endRDF [this]
+      (.endQueryResult result-writer))
+    (handleNamespace [this prefix uri]
+      ;; No op
+      )
+    (handleStatement [this statement]
+      (let [s (.getSubject statement)
+            p (.getPredicate statement)
+            o (.getObject statement)
+            bs (doto (MapBindingSet.)
+                 (.addBinding "s" s)
+                 (.addBinding "p" p)
+                 (.addBinding "o" o))]
+        (.handleSolution result-writer bs)))
+    (handleComment [this comment]
+      ;; No op
+      )))
+
+(defn- create-rdf-writer-factory [rdf-format writer-fn]
+  (reify RDFWriterFactory
+    (getRDFFormat [this] rdf-format)
+    (^RDFWriter getWriter [this ^OutputStream os]
+      (query-result-writer->rdf-writer (writer-fn)))
+    (^RDFWriter getWriter [this ^Writer w]
+      (throw (RuntimeException. "Not supported - use OutputStream overload")))))
+
+(defn- get-rdf-writer-registry []
+  (RDFWriterRegistry/getInstance))
+
+(defn- register-rdf-writer-factory [writer-factory]
+  (.add (get-rdf-writer-registry) writer-factory))
+
+(defn- create-csv-result-writer [output-stream]
+  (SPARQLResultsCSVWriter. output-stream))
+
+(defn- create-tsv-result-writer [output-stream]
+  (SPARQLResultsTSVWriter. output-stream))
+
+(register-rdf-writer-factory (create-rdf-writer-factory csv-rdf-format create-csv-result-writer))
+(register-rdf-writer-factory (create-rdf-writer-factory tsv-rdf-format create-tsv-result-writer))
+
+(defn- format-preferences->mime-spec [format-prefs]
+  (into {} (mapcat (fn [[f q]] (map (fn [m] [m [f q]]) (.getMIMETypes f))) format-prefs)))
+
+(def graph-query-mime-spec
+  (merge (format-preferences->mime-spec
+          {RDFFormat/NTRIPLES 1.0
+           RDFFormat/NQUADS 0.9
+           RDFFormat/N3 0.8
+           RDFFormat/TRIG 0.8
+           RDFFormat/TRIX 0.8
+           RDFFormat/TURTLE 0.9
+           RDFFormat/RDFXML 0.9
+           RDFFormat/RDFJSON 0.9
+           csv-rdf-format 0.8
+           tsv-rdf-format 0.7})
+         {"text/html" [RDFFormat/TURTLE 0.8]}))
+
+(def boolean-query-mime-spec
+  (merge (format-preferences->mime-spec
+          {BooleanQueryResultFormat/SPARQL 1.0
+           BooleanQueryResultFormat/JSON 1.0
+           BooleanQueryResultFormat/TEXT 0.9})
+         {"text/html" [BooleanQueryResultFormat/TEXT 0.8]
+          "text/plain" [BooleanQueryResultFormat/TEXT 0.9]}))
+
+(def tuple-query-mime-spec
+  (merge (format-preferences->mime-spec
+          {TupleQueryResultFormat/CSV 1.0
+           TupleQueryResultFormat/JSON 0.9
+           TupleQueryResultFormat/SPARQL 0.9
+           TupleQueryResultFormat/BINARY 0.7
+           TupleQueryResultFormat/TSV 0.7})
+         {"text/plain" [TupleQueryResultFormat/CSV 1.0]
+          "text/html" [TupleQueryResultFormat/CSV 1.0]}))
+
+(defn- mime-pref [mime q] [mime :as mime :qs q])
+
+(defn- mime-spec->mime-preferences [spec]
+  (vec (mapcat (fn [[m [_ q]]] (mime-pref m q)) spec)))
+
+(defn- get-mime-spec [query-type]
+  (case query-type
+    :select tuple-query-mime-spec
+    :ask boolean-query-mime-spec
+    :construct graph-query-mime-spec
+    nil))
+
+(defn negotiate
+  "Returns the preferred sesame query result format to use for the
+  client's accepted mime types. The format returned depends on the
+  type of the query (:ask, :select or :construct) to be executed. If
+  no acceptable format is available, nil is returned."
+  [query-type accept]
+  (if-let [spec (get-mime-spec query-type)]
+    (let [prefs (mime-spec->mime-preferences spec)
+          request {:headers {"accept" accept}}
+          handler (wrap-accept identity {:mime prefs})]
+      (if-let [mime (get-in (handler request) [:accept :mime])]
+        (let [[format _] (get spec mime)]
+          format)))))
