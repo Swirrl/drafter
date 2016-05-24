b5ee833a76d6d753df619fd0907a69bc203a375d
lkitching <lee@swirrl.com>
2016-05-24 15:42:29 +0100

    Fix remote backend protocol implementations.

--- src/drafter/backend/sesame/remote.clj
+++ src/drafter/backend/sesame/remote.clj
@@ -11,12 +11,13 @@
 
 (extend drafter.backend.sesame.remote.SesameRemoteSparqlBackend
   proto/ITripleReadable default-triple-readable-impl
+  proto/ITripleWriteable default-triple-writeable-impl
   proto/ISPARQLable default-sparqlable-impl
   proto/ISPARQLUpdateable default-isparql-updatable-impl
   SparqlExecutor default-sparql-query-impl
   QueryRewritable default-query-rewritable-impl
   SparqlUpdateExecutor sparql-impl/sparql-update-executor-impl
-  DraftManagement sparql-impl/sparql-update-executor-impl
+  DraftManagement sparql-impl/draft-management-impl
   ApiOperations default-api-operations-impl
   Stoppable default-stoppable-impl
   sesproto/ToRepository {:->sesame-repo :repo}
