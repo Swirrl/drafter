58946c1d1a8ece1e42c7b1e027be87480f30f833
Rick Moynihan <rick.m@swirrl.com>
2016-08-26 14:16:05 +0100

    Never use RDF+XML and reduce the range of options available for content negotiation.
    This prevents running into this issue with RDF+XML:
    
    https://www.w3.org/TR/REC-rdf-syntax/#section-Serialising
    
    In particular some of our URIs on scotland look like this:
    
    http://blah.com/blah/blah/blah/(something-in-parentheses)
    
    This *is* a legal URI/URL and it will work with RDF+XML for anything
    that isn't a predicate.  RDF+XML can't serialise predicates that contain
    parentheses in the trailing URL path.  Madness!!!

--- src/drafter/backend/sesame/remote/repository.clj
+++ src/drafter/backend/sesame/remote/repository.clj
@@ -2,13 +2,14 @@
   (:require [clojure.string :as str]
             [clojure.tools.logging :as log]
             [grafter.rdf.repository.registry :as reg])
-  (:import [drafter.rdf DrafterSPARQLRepository]
-           [java.nio.charset Charset]
-           [org.openrdf.rio RDFParserRegistry RDFFormat]
-           [org.openrdf.query.resultio TupleQueryResultFormat BooleanQueryResultFormat
-            TupleQueryResultParserRegistry
-            BooleanQueryResultParserRegistry]
-           [org.openrdf.query.resultio.text.csv SPARQLResultsCSVParserFactory]))
+  (:import drafter.rdf.DrafterSPARQLRepository
+           org.openrdf.query.resultio.sparqljson.SPARQLResultsJSONParserFactory
+           org.openrdf.query.resultio.sparqlxml.SPARQLResultsXMLParserFactory
+           org.openrdf.rio.nquads.NQuadsParserFactory
+           org.openrdf.query.resultio.sparqljson.SPARQLBooleanJSONParserFactory
+           org.openrdf.query.resultio.text.BooleanTextParserFactory
+           org.openrdf.query.resultio.sparqlxml.SPARQLBooleanXMLParserFactory
+           org.openrdf.rio.ntriples.NTriplesParserFactory))
 
 (defn get-required-environment-variable [var-key env-map]
   (if-let [ev (var-key env-map)]
@@ -33,14 +34,37 @@
       (log/info "Initialised repo at QUERY=" query-endpoint ", UPDATE=" update-endpoint)
       repo))
 
+
+;; Set some whitelists that ensure we're much more strict around what
+;; formats we negotiate with stardog.  If you want to run drafter
+;; against another (non stardog) store we should configure these to be
+;; different.
+;;
+;; For construct we avoid Turtle because of Stardog bug #3087
+;; (https://complexible.zendesk.com/hc/en-us/requests/524)
+;;
+;; Also we avoid RDF+XML because RDF+XML can't even represent some RDF graphs:
+;; https://www.w3.org/TR/REC-rdf-syntax/#section-Serialising which
+;; causes us some issues when URI's for predicates contain parentheses.
+;;
+;; Ideally we'd just run with sesame's defaults, but providing a
+;; smaller list should mean less bugs in production as we can choose
+;; the most reliable formats and avoid those with known issues.
+;;
+(def construct-formats-whitelist #{NTriplesParserFactory NQuadsParserFactory})
+(def select-formats-whitelist #{SPARQLResultsXMLParserFactory SPARQLResultsJSONParserFactory})
+(def ask-formats-whitelist #{SPARQLBooleanJSONParserFactory BooleanTextParserFactory SPARQLBooleanXMLParserFactory})
+
 (defn create-repository-for-environment [env-map]
   "Creates a new SPARQL repository with the query and update endpoints
   configured in the given environment map."
 
-  (let [updated-registries (update (reg/registered-parser-factories)
-                                   ;; Force removal of TurtleParser
-                                   :construct #(disj % org.openrdf.rio.turtle.TurtleParserFactory))]
-    (reg/register-parser-factories! updated-registries))
+  ;; This call here obliterates the sesame defaults for registered
+  ;; parsers.  Forcing content negotiation to work only with the
+  ;; parsers we explicitly whitelist above.
+  (reg/register-parser-factories! {:select select-formats-whitelist
+                                   :construct construct-formats-whitelist
+                                   :ask ask-formats-whitelist})
 
   (let [query-endpoint (get-required-environment-variable :sparql-query-endpoint env-map)
         update-endpoint (get-required-environment-variable :sparql-update-endpoint env-map)]
