b952d943c566b5ef9f77f406aac6954a599927fd
Alex Lynham <acelynham@hotmail.com>
2017-02-13 15:58:28 +0000

    Hopefully now point to correct dawg location

--- travis/bootstrap_stardog.sh
+++ travis/bootstrap_stardog.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 WORKING_DIR=`pwd`
-echo "Working dir: $WORKING_DIR"
+echo "> Working dir: $WORKING_DIR"
 
 # install stardog
 sudo mkdir -p /opt/stardog/releases
@@ -9,19 +9,17 @@ sudo mkdir -p /var/lib/stardog-home
 sudo mkdir -p /opt/stardog/stardog
 
 STARDOG_HOME=/var/lib/stardog-home
-echo "Stardog home: $STARDOG_HOME"
+echo "> Stardog home: $STARDOG_HOME"
 
-echo 'Downloading and unzipping Stardog...'
+echo '> Downloading and unzipping Stardog...'
 cd /opt/stardog/releases
 sudo curl -O https://stardog-versions.s3.amazonaws.com/stardog-4.1.2.zip
 sudo unzip stardog-4.1.2.zip
 
-echo 'Adding license...'
+echo '> Adding license...'
 sudo cp $WORKING_DIR/travis/stardog-license-key.bin /var/lib/stardog-home
 sudo cp $WORKING_DIR/travis/stardog.properties /var/lib/stardog-home
 sudo ln -s /opt/stardog/releases/stardog-4.1.2 /opt/stardog/stardog
 
 # start stardog
-ls /opt/stardog/stardog/stardog-4.1.2
-ls /opt/stardog/stardog/stardog-4.1.2/bin
-/opt/stardog/stardog/bin/stardog-admin server start --disable-security
+/opt/stardog/stardog/stardog-4.1.2/bin/stardog-admin server start --disable-security
