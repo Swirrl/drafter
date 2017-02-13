e1e99869fa3d27d1ba12003023bf5dab9fdc64f1
Alex Lynham <acelynham@hotmail.com>
2017-02-13 15:45:16 +0000

    Some more location changes.

--- travis/bootstrap_stardog.sh
+++ travis/bootstrap_stardog.sh
@@ -8,6 +8,9 @@ sudo mkdir -p /opt/stardog/releases
 sudo mkdir -p /var/lib/stardog-home
 sudo mkdir -p /opt/stardog/stardog
 
+STARDOG_HOME=/var/lib/stardog-home
+echo "Stardog home: $STARDOG_HOME"
+
 echo 'Downloading and unzipping Stardog...'
 cd /opt/stardog/releases
 sudo curl -O https://stardog-versions.s3.amazonaws.com/stardog-4.1.2.zip
@@ -15,10 +18,10 @@ sudo unzip stardog-4.1.2.zip
 
 echo 'Adding license...'
 sudo cp $WORKING_DIR/travis/stardog-license-key.bin /var/lib/stardog-home
-sudo cp $WORKING_DIR/travis/stardog-properties /var/lib/stardog-home
+sudo cp $WORKING_DIR/travis/stardog.properties /var/lib/stardog-home
 sudo ln -s /opt/stardog/releases/stardog-4.1.2 /opt/stardog/stardog
 
 # start stardog
-ls /opt/stardog/stardog/
-ls /opt/stardog/stardog/bin
+ls /opt/stardog/stardog/stardog-4.1.2
+ls /opt/stardog/stardog/stardog-4.1.2/bin
 /opt/stardog/stardog/bin/stardog-admin server start --disable-security
