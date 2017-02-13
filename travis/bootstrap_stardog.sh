8d63873e52e9da27bb1fd780e62e90efc95eea36
Alex Lynham <acelynham@hotmail.com>
2017-02-13 15:24:39 +0000

    Fix working dir error.

--- travis/bootstrap_stardog.sh
+++ travis/bootstrap_stardog.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+WORKING_DIR=`pwd`
+echo "Working dir: $WORKING_DIR"
+
 # install stardog
 sudo mkdir -p /opt/stardog/releases
 sudo mkdir -p /var/lib/stardog-home
@@ -11,9 +14,11 @@ sudo curl -O https://stardog-versions.s3.amazonaws.com/stardog-4.1.2.zip
 sudo unzip stardog-4.1.2.zip
 
 echo 'Adding license...'
-sudo cp ./travis/stardog-license-key.bin /var/lib/stardog-home
-sudo cp ./travis/stardog-properties /var/lib/stardog-home
+sudo cp $WORKING_DIR/travis/stardog-license-key.bin /var/lib/stardog-home
+sudo cp $WORKING_DIR/travis/stardog-properties /var/lib/stardog-home
 sudo ln -s /opt/stardog/releases/stardog-4.1.2 /opt/stardog/stardog
 
 # start stardog
+ls /opt/stardog/stardog/
+ls /opt/stardog/stardog/bin
 /opt/stardog/stardog/bin/stardog-admin server start --disable-security
