0ea1e337b50f86ab20007341b7c452cfd8e753c3
Rick Moynihan <rick.m@swirrl.com>
2017-03-10 11:49:51 +0000

    Refactor states as separate functions when adding data in draftset

--- src/drafter/rdf/draftset_management.clj
+++ src/drafter/rdf/draftset_management.clj
@@ -562,49 +562,63 @@
     {:graph-uri graph-uri :triples (map map->Triple quads)}
     (throw (IllegalArgumentException. "Quad batch must contain at least one item"))))
 
+(declare append-draftset-quads)
+
+(defn- append-draftset-quads*
+  [quad-batches live->draft backend job-started-at job draftset-ref state]
+  (if-let [batch (first quad-batches)]
+    (let [{:keys [graph-uri triples]} (quad-batch->graph-triples batch)]
+      (if-let [draft-graph-uri (get live->draft graph-uri)]
+        (do
+          (mgmt/set-modifed-at-on-draft-graph! backend draft-graph-uri job-started-at)
+          (mgmt/append-data-batch! backend draft-graph-uri triples)
+          (let [next-job (create-child-job
+                          job
+                          (partial append-draftset-quads backend draftset-ref live->draft (rest quad-batches) (merge state {:op :append})))]
+            (scheduler/queue-job! next-job)))
+        ;;NOTE: do this immediately instead of scheduling a
+        ;;continuation since we haven't done any real work yet
+        (append-draftset-quads backend draftset-ref live->draft quad-batches (merge state {:op :copy-graph :graph graph-uri}) job)))
+    (job-succeeded! job)))
+
+(defn- copy-graph-for-append*
+  [state draftset-ref backend live->draft quad-batches job]
+  (let [live-graph-uri (:graph state)
+        ds-uri (str (ds/->draftset-uri draftset-ref))
+        {:keys [draft-graph-uri graph-map]} (mgmt/ensure-draft-exists-for backend live-graph-uri live->draft ds-uri)
+        clone-batches (jobs/get-graph-clone-batches backend live-graph-uri)
+        copy-batches-state (merge state {:op :copy-graph-batches
+                                         :graph live-graph-uri
+                                         :draft-graph draft-graph-uri
+                                         :batches clone-batches})]
+    ;;NOTE: do this immediately since we still haven't done any real work yet...
+    (append-draftset-quads backend draftset-ref graph-map quad-batches copy-batches-state job)))
+
+(defn- copy-graph-batches-for-append*
+  [state backend job draftset-ref live->draft quad-batches]
+  (let [{:keys [graph batches draft-graph]} state]
+    (if-let [[offset limit] (first batches)]
+      (do
+        (jobs/copy-graph-batch! backend graph draft-graph offset limit)
+        (let [next-state (update-in state [:batches] rest)
+              next-job (create-child-job
+                        job
+                        (partial append-draftset-quads backend draftset-ref live->draft quad-batches next-state))]
+          (scheduler/queue-job! next-job)))
+      ;;graph copy completed so continue appending quads
+      ;;NOTE: do this immediately since we haven't done any work on this iteration
+      (append-draftset-quads backend draftset-ref live->draft quad-batches (merge state {:op :append}) job))))
+
 (defn- append-draftset-quads [backend draftset-ref live->draft quad-batches {:keys [op job-started-at] :as state} job]
   (case op
     :append
-    (if-let [batch (first quad-batches)]
-      (let [{:keys [graph-uri triples]} (quad-batch->graph-triples batch)]
-        (if-let [draft-graph-uri (get live->draft graph-uri)]
-          (do
-            (mgmt/set-modifed-at-on-draft-graph! backend draft-graph-uri job-started-at)
-            (mgmt/append-data-batch! backend draft-graph-uri triples)
-            (let [next-job (create-child-job
-                            job
-                            (partial append-draftset-quads backend draftset-ref live->draft (rest quad-batches) (merge state {:op :append})))]
-              (scheduler/queue-job! next-job)))
-          ;;NOTE: do this immediately instead of scheduling a
-          ;;continuation since we haven't done any real work yet
-          (append-draftset-quads backend draftset-ref live->draft quad-batches (merge state {:op :copy-graph :graph graph-uri}) job)))
-      (job-succeeded! job))
+    (append-draftset-quads* quad-batches live->draft backend job-started-at job draftset-ref state)
 
     :copy-graph
-    (let [live-graph-uri (:graph state)
-          ds-uri (str (ds/->draftset-uri draftset-ref))
-          {:keys [draft-graph-uri graph-map]} (mgmt/ensure-draft-exists-for backend live-graph-uri live->draft ds-uri)
-          clone-batches (jobs/get-graph-clone-batches backend live-graph-uri)
-          copy-batches-state (merge state {:op :copy-graph-batches
-                                           :graph live-graph-uri
-                                           :draft-graph draft-graph-uri
-                                           :batches clone-batches})]
-      ;;NOTE: do this immediately since we still haven't done any real work yet...
-      (append-draftset-quads backend draftset-ref graph-map quad-batches copy-batches-state job))
+    (copy-graph-for-append* state draftset-ref backend live->draft quad-batches job)
 
     :copy-graph-batches
-    (let [{:keys [graph batches draft-graph]} state]
-      (if-let [[offset limit] (first batches)]
-        (do
-          (jobs/copy-graph-batch! backend graph draft-graph offset limit)
-          (let [next-state (update-in state [:batches] rest)
-                next-job (create-child-job
-                          job
-                          (partial append-draftset-quads backend draftset-ref live->draft quad-batches next-state))]
-            (scheduler/queue-job! next-job)))
-        ;;graph copy completed so continue appending quads
-        ;;NOTE: do this immediately since we haven't done any work on this iteration
-        (append-draftset-quads backend draftset-ref live->draft quad-batches (merge state {:op :append}) job)))))
+    (copy-graph-batches-for-append* state backend job draftset-ref live->draft quad-batches)))
 
 (defn- append-quads-to-draftset-job [backend draftset-ref quads]
   (let [graph-map (get-draftset-graph-mapping backend draftset-ref)
