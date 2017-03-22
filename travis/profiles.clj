d84eab4c0e7f3630b761551226a1e043e8210a8e
Alex Lynham <acelynham@hotmail.com>
2017-03-22 17:03:25 +0000

    Modify gitingore so it ignores a profiles.clj file in project root but not the travis dir.

---
+++ travis/profiles.clj
@@ -0,0 +1,11 @@
+{:repositories
+  [["swirrl-jars-snapshots"
+    {:url "s3p://swirrl-jars/snapshots/"
+     :username :env
+     :passphrase :env
+     :releases false}]
+   ["swirrl-jars-releases"
+    {:url "s3p://swirrl-jars/releases/"
+     :username :env
+     :passphrase :env
+     :snapshots false}]]}
