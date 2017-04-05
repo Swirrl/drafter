429e4824a54dbf8432ac2059887f10593c904d49
Rick Moynihan <rick.m@swirrl.com>
2017-04-05 12:29:30 +0100

    tidy up project.clj by sorting deps with cljr-sort-project-dependencies

--- project.clj
+++ project.clj
@@ -4,13 +4,9 @@
   :license {:name "Proprietary & Commercially Licensed Only"
             :url "http://swirrl.com/"}
 
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.openrdf.sesame/sesame-queryrender "2.8.11"]
-                 [org.openrdf.sesame/sesame-runtime "2.8.11"]
-                 [org.openrdf.sesame/sesame-queryresultio-sparqlxml "2.8.11"]
-
-                 [clj-yaml "0.4.0"]      ;; for loading our Swagger schemas
-                 [metosin/scjsv "0.3.0"] ;; for validating our Swagger/JSON schemas
+  :dependencies [[buddy/buddy-auth "0.9.0"]
+                 [buddy/buddy-core "0.9.0"]
+                 [clj-yaml "0.4.0"] ;; for loading our Swagger schemas
 
                  ;; Lock dependency of jackson to a version that
                  ;; works with sesame's sparql json results renderer
@@ -19,46 +15,41 @@
                  ;; NOTE: When we upgrade sesame to RDF4j we can possibly
                  ;; drop this override.
                  [com.fasterxml.jackson.core/jackson-core "2.6.7"]
+                 [com.novemberain/monger "3.0.2"]
+                 [com.sun.mail/javax.mail "1.5.5"]
+                 [com.taoensso/tower "2.0.2"]
+                 [drafter-client "0.3.6-SNAPSHOT"]
+                 [environ "1.0.0"]
+                 [grafter "0.7.5"]
+                 [grafter/url "0.2.1"]
+                 [grafter/vocabularies "0.1.3"]
+                 [lib-noir "0.9.9" :exclusions [compojure org.clojure/java.classpath org.clojure/tools.reader org.clojure/java.classpath]]
+                 [me.raynes/fs "1.4.6"] ;; filesystem utils
+                 [metosin/ring-swagger-ui "2.1.4-0"]
+                 [metosin/scjsv "0.3.0"] ;; for validating our Swagger/JSON schemas
+
 
                  ;; Use JENA for our query rewriting
                  [org.apache.jena/jena-arq "3.1.1" :exclusions [org.slf4j/slf4j-api
                                                                 org.slf4j/jcl-over-slf4j
                                                                 org.apache.httpcomponents/httpclient]]
-                 [org.apache.jena/jena-core "3.1.1" :exclusions [org.slf4j/slf4j-api]]
                  [org.apache.jena/jena-base "3.1.1" :exclusions [org.slf4j/slf4j-api]]
+                 [org.apache.jena/jena-core "3.1.1" :exclusions [org.slf4j/slf4j-api]]
                  [org.apache.jena/jena-iri "3.1.1" :exclusions [org.slf4j/slf4j-api]]
-
-                 [me.raynes/fs "1.4.6"] ;; filesystem utils
-                 [lib-noir "0.9.9" :exclusions [compojure org.clojure/java.classpath org.clojure/tools.reader org.clojure/java.classpath]]
-                 [ring "1.4.0" :exclusions [org.clojure/java.classpath]]
-                 [ring/ring-core "1.4.0"]
-                 [ring-server "0.4.0"]
-                 [wrap-verbs "0.1.1"]
-                 [selmer "0.6.9"]
-                 [com.novemberain/monger "3.0.2"]
-
-                 [swirrl/lib-swirrl-server "0.6.3" :exclusions [clout org.clojure/java.classpath]]
-
-                 [buddy/buddy-core "0.9.0"]
-                 [buddy/buddy-auth "0.9.0"]
+                 [org.clojure/clojure "1.8.0"]
                  [org.mindrot/jbcrypt "0.3m"]
-
-                 [grafter "0.7.5"]
-
-                 [grafter/vocabularies "0.1.3"]
-                 [grafter/url "0.2.1"]
-
-                 [com.taoensso/tower "2.0.2"]
+                 [org.openrdf.sesame/sesame-queryrender "2.8.11"]
+                 [org.openrdf.sesame/sesame-queryresultio-sparqlxml "2.8.11"]
+                 [org.openrdf.sesame/sesame-runtime "2.8.11"]
                  [org.slf4j/slf4j-log4j12 "1.7.9" :exclusions [log4j org.slf4j/slf4j-api]]
-                 [ring-middleware-accept "2.0.3"]
-                 [environ "1.0.0"]
-
                  [prismatic/schema "1.0.4"]
-                 [drafter-client "0.3.6-SNAPSHOT"]
-
-                 [metosin/ring-swagger-ui "2.1.4-0"]
-
-                 [com.sun.mail/javax.mail "1.5.5"]]
+                 [ring "1.4.0" :exclusions [org.clojure/java.classpath]]
+                 [ring-middleware-accept "2.0.3"]
+                 [ring-server "0.4.0"]
+                 [ring/ring-core "1.4.0"]
+                 [selmer "0.6.9"]
+                 [swirrl/lib-swirrl-server "0.6.3" :exclusions [clout org.clojure/java.classpath]]
+                 [wrap-verbs "0.1.1"]]
 
   ;; Ensure we build the java sub project source code too!
   :java-source-paths ["src-java/drafter_sparql_repository/src/main/java"]
@@ -102,11 +93,11 @@
                        :auto-reload?  false}}
 
 
-   :perforate { :dependencies [[perforate "0.3.4"] ;; include perforate and criterium in repl environments
+   :perforate { :dependencies [[clj-http "1.1.0"]
                                [criterium "0.4.3"] ;; for easy benchmarking
-                               [clj-http "1.1.0"]
                                [drafter-client "0.3.6-SNAPSHOT"]
                                [grafter "0.6.0-alpha5"]
+                               [perforate "0.3.4"] ;; include perforate and criterium in repl environments
                                ]}
 
    :dev [:dev-common :dev-overrides]
@@ -117,16 +108,16 @@
                 :source-paths ["dev/clj"]
                 :resource-paths ["dev/resources"]
 
-                :dependencies [[ring-mock "0.1.5"]
-                               [com.aphyr/prism "0.1.1" :exclusions [org.clojure/clojure]]
-                               [org.clojure/data.json "0.2.5"]
+                :dependencies [ ;;[clj-http "1.1.0"]
                                [clojure-csv/clojure-csv "2.0.1"]
-                               [ring/ring-devel "1.3.2" :exclusions [org.clojure/java.classpath org.clojure/tools.reader]]
-                               ;;[perforate "0.3.4"] ;; include perforate and criterium in repl environments
+                               [com.aphyr/prism "0.1.1" :exclusions [org.clojure/clojure]]
                                ;;[criterium "0.4.3"] ;; for easy benchmarking
-                               ;;[clj-http "1.1.0"]
                                ;;[drafter-client "0.3.6-SNAPSHOT"]
-                               [prismatic/schema "1.0.4"]]
+                               [org.clojure/data.json "0.2.5"]
+                               ;;[perforate "0.3.4"] ;; include perforate and criterium in repl environments
+                               [prismatic/schema "1.0.4"]
+                               [ring-mock "0.1.5"]
+                               [ring/ring-devel "1.3.2" :exclusions [org.clojure/java.classpath org.clojure/tools.reader]]]
 
                 ;;:env {:dev true}
                 ;;:jvm-opts ["-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005"]
@@ -142,7 +133,7 @@
              ;;"-Dhttp.maxConnections=1"
              ]
 
-  ;NOTE: expected JVM version to run against is defined in the Dockerfile
+                                        ;NOTE: expected JVM version to run against is defined in the Dockerfile
   :javac-options ["-target" "7" "-source" "7"]
   :min-lein-version "2.5.0"
 
@@ -154,5 +145,4 @@
                   ["change" "version" "leiningen.release/bump-version"]
                   ["vcs" "commit"]
                   ;;["vcs" "push"]
-                  ]
-  )
+                  ])
