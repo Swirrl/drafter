5dbaed8a39c01b444c588088fd48251793832c29
RicSwirrl <ric@swirrl.com>
2017-06-14 10:46:07 +0100

    Version 2.1.3-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject drafter "2.1.2"
+(defproject drafter "2.1.3-SNAPSHOT"
   :description "Backend PMD service"
   :url "http://github.com/Swirrl/drafter"
   :license {:name "Proprietary & Commercially Licensed Only"
