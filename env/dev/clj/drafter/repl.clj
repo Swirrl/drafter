aacf52c947ec01d0d1db49d5ed160cf9937888bd
Rick Moynihan <rick.m@swirrl.com>
2017-10-25 14:15:26 +0100

    Move repl namespace into dev path

---
+++ env/dev/clj/drafter/repl.clj
@@ -0,0 +1,21 @@
+(ns drafter.repl
+  (:require [drafter.main :refer [start-system! stop-system!]]
+            [eftest.runner :as eftest]))
+
+(defn run-tests []
+  (eftest/run-tests (eftest/find-tests "test") {:multithread? false}))
+
+(do (println)
+    (println "   ___           _____         ")
+    (println "  / _ \\_______ _/ _/ /____ ____")
+    (println " / // / __/ _ `/ _/ __/ -_) __/")
+    (println "/____/_/  \\_,_/_/ \\__/\\__/_/   ")
+    (println)
+    (println "Welcome to the Drafter REPL!")
+    (println)
+    (println)
+    (println "REPL Commands: ")
+    (println)
+    (println "(start-system!)")
+    (println "(stop-system!)")
+    (println "(run-tests)"))
