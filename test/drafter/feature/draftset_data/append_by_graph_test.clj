12dbac4d70354201ebce941fd4ec578df861b529
Rick Moynihan <rick.m@swirrl.com>
2018-01-17 13:19:56 +0000

    Update ns names for moved namespaces

--- test/drafter/feature/draftset_data/append_by_graph_test.clj
+++ test/drafter/feature/draftset_data/append_by_graph_test.clj
@@ -1,4 +1,4 @@
-(ns drafter.feature.draftset.append-by-graph-test
-  (:require [drafter.feature.draftset.append :as sut]
+(ns drafter.feature.draftset-data.append-by-graph-test
+  (:require [drafter.feature.draftset-data.append :as sut]
             [clojure.test :as t]))
 
