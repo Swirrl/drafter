ccba5882cc08563cfa47f6a4b175754190b5f653
Rick Moynihan <rick.m@swirrl.com>
2018-02-01 13:37:17 +0000

    move sudo up to outer script

--- travis/bootstrap_stardog.sh
+++ travis/bootstrap_stardog.sh
@@ -4,9 +4,9 @@ export WORKING_DIR=`pwd`
 echo "> Working dir: $WORKING_DIR"
 
 # install stardog
-sudo mkdir -p /opt/stardog/releases
-sudo mkdir -p /var/lib/stardog-home
-sudo mkdir -p /opt/stardog/stardog
+mkdir -p /opt/stardog/releases
+mkdir -p /var/lib/stardog-home
+mkdir -p /opt/stardog/stardog
 
 export STARDOG_HOME=/var/lib/stardog-home
 echo "> Stardog home: $STARDOG_HOME"
@@ -15,18 +15,19 @@ echo '> Downloading and unzipping Stardog...'
 cd /opt/stardog/releases
 sudo curl -O https://stardog-versions.s3.amazonaws.com/$STARDOG_VERSION.zip
 sudo unzip $STARDOG_VERSION.zip
+unzip $STARDOG_VERSION.zip
 cd $WORKING_DIR
 
 echo "> Permissions.."
-sudo chown -R travis /opt/stardog/releases
-sudo chmod +wx /opt/stardog/releases
-sudo chown -R travis $STARDOG_HOME
-sudo chmod +wx $STARDOG_HOME
+chown -R travis /opt/stardog/releases
+chmod +wx /opt/stardog/releases
+chown -R travis $STARDOG_HOME
+chmod +wx $STARDOG_HOME
 
 echo '> Adding license...'
-sudo cp $WORKING_DIR/travis/stardog-license-key.bin /var/lib/stardog-home
-sudo cp $WORKING_DIR/travis/stardog.properties /var/lib/stardog-home
-sudo ln -s /opt/stardog/releases/$STARDOG_VERSION /opt/stardog/stardog
+cp $WORKING_DIR/travis/stardog-license-key.bin /var/lib/stardog-home
+cp $WORKING_DIR/travis/stardog.properties /var/lib/stardog-home
+ln -s /opt/stardog/releases/$STARDOG_VERSION /opt/stardog/stardog
 
 # start stardog
 /opt/stardog/stardog/$STARDOG_VERSION/bin/stardog-admin server start --disable-security
