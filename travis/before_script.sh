1c7834f3b18c628697190c2794804c8e5fe36833
Rick Moynihan <rick.m@swirrl.com>
2018-02-01 14:26:23 +0000

    Remove stardog 4 command line option to specify disk type db.
    This option is not supported on stardog 5.

--- travis/before_script.sh
+++ travis/before_script.sh
@@ -2,6 +2,6 @@
 
 sleep 15
 
-env JAVA_HOME=/usr/lib/jvm/java-8-oracle/bin/java /opt/stardog/stardog/$STARDOG_VERSION/bin/stardog-admin db create -n drafter-test-db -t D -o strict.parsing=false -o query.all.graphs=true -o reasoning.type=none --
+env JAVA_HOME=/usr/lib/jvm/java-8-oracle/bin/java /opt/stardog/stardog/$STARDOG_VERSION/bin/stardog-admin db create -n drafter-test-db -o strict.parsing=false -o query.all.graphs=true -o reasoning.type=none --
 
 sleep 10
