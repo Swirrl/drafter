7a781a3cb76640b130fe59df5d21f0880f484b3d
Dan Midwood <dan@swirrl.com>
2018-04-23 09:03:16 +0100

    Comment out test for commented out code

--- test/drafter/stasher/cache_clearer_test.clj
+++ test/drafter/stasher/cache_clearer_test.clj
@@ -2,14 +2,14 @@
   (:require [drafter.stasher.cache-clearer :as sut]
             [clojure.test :as t]))
 
-(t/deftest closest-test
+#_(t/deftest closest-test
 
-  (t/is (= [5 4 4 2 1]
-           (sut/sorted-by-distance 5 [1 5 2 4 4])))
+    (t/is (= [5 4 4 2 1]
+             (sut/sorted-by-distance 5 [1 5 2 4 4])))
   
-  (t/are [target expected items] (= expected (first (sut/sorted-by-distance target items)))
-    5 5 [1 2 3 4 5 6 7 8 9 0]
-    5 4 [1 2 2 4 9 8]
-    5 2 [10 11 12 13 14 2]
-    5 10 [10 10 10 10 10 10]
-    10 nil []))
+    (t/are [target expected items] (= expected (first (sut/sorted-by-distance target items)))
+      5 5 [1 2 3 4 5 6 7 8 9 0]
+      5 4 [1 2 2 4 9 8]
+      5 2 [10 11 12 13 14 2]
+      5 10 [10 10 10 10 10 10]
+      10 nil []))
