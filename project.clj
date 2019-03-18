5fe150bd974700e020d2120648d3b6e85a13e4bb
Andrew Mcveigh <me@andrewmcveigh.com>
2019-03-18 13:44:49 +0000

    Update dependencies (clj 1.10.0, etc)

--- project.clj
+++ project.clj
@@ -26,23 +26,23 @@
                 :dev :dev}
 
   :dependencies [[buddy/buddy-auth "2.1.0"]
-                 [buddy/buddy-core "1.4.0"]
+                 [buddy/buddy-core "1.5.0"]
 
-                 [org.clojure/clojure "1.9.0"]
-                 [org.clojure/spec.alpha "0.1.134"]
+                 [org.clojure/clojure "1.10.0"]
+                 [org.clojure/spec.alpha "0.2.176"]
                  
                  [org.clojure/math.combinatorics "0.1.4"]
                  
                  [cognician/dogstatsd-clj "0.1.2"]
 
-                 [commons-codec "1.11"]
+                 [commons-codec "1.12"]
                  
                  [clj-yaml "0.4.0"] ;; for loading our Swagger schemas
-                 [metosin/scjsv "0.4.0"] ;; for validating our Swagger/JSON schemas
+                 [metosin/scjsv "0.5.0"] ;; for validating our Swagger/JSON schemas
 
-                 [aero "1.1.2"]
+                 [aero "1.1.3"]
 
-                 [integrant "0.6.3"]
+                 [integrant "0.7.0"]
                  
                  ;; Lock dependency of jackson to a version that
                  ;; works with sesame's sparql json results renderer
@@ -53,11 +53,11 @@
                  ;;
                  ;; Without this you get errors like:
                  ;; java.lang.NoClassDefFoundError: com/fasterxml/jackson/core/FormatFeature, compiling:(cheshire/factory.clj:54:7)
-                 [com.fasterxml.jackson.core/jackson-core "2.6.7"]
+                 [com.fasterxml.jackson.core/jackson-core "2.9.8"]
                  
-                 [com.novemberain/monger "3.1.0"]
+                 [com.novemberain/monger "3.5.0"]
 
-                 [com.sun.mail/javax.mail "1.6.0"]
+                 [com.sun.mail/javax.mail "1.6.2"]
                  ;;[com.taoensso/tower "2.0.2"]
 
                  [grafter "0.11.0.2-drafter-rdf4j"]
@@ -67,30 +67,30 @@
                  [lib-noir "0.9.9" :exclusions [compojure org.clojure/java.classpath org.clojure/tools.reader org.clojure/java.classpath]]
                  [me.raynes/fs "1.4.6"] ;; filesystem utils
 
-                 [metosin/ring-swagger-ui "2.2.10"]
+                 [metosin/ring-swagger-ui "3.20.1"]
 
                  ;; Use JENA for our query rewriting
                  ;; Use private release of Jena based on commit 44b478ea8637ca16035e56ef49d6cb6e59abc289
                  ;; See https://github.com/Swirrl/drafter/issues/270
                  ;; This should be updated to use 3.9.0 when that version is released
-                 [org.apache.jena/jena-arq "3.9.0-44b478ea" :exclusions [org.slf4j/slf4j-api
+                 [org.apache.jena/jena-arq "3.10.0" :exclusions [org.slf4j/slf4j-api
                                                                          org.slf4j/jcl-over-slf4j
                                                                          org.apache.httpcomponents/httpclient]]
-                 [org.apache.jena/jena-base "3.9.0-44b478ea" :exclusions [org.slf4j/slf4j-api]]
-                 [org.apache.jena/jena-core "3.9.0-44b478ea" :exclusions [org.slf4j/slf4j-api]]
-                 [org.apache.jena/jena-iri "3.9.0-44b478ea" :exclusions [org.slf4j/slf4j-api]]
+                 [org.apache.jena/jena-base "3.10.0" :exclusions [org.slf4j/slf4j-api]]
+                 [org.apache.jena/jena-core "3.10.0" :exclusions [org.slf4j/slf4j-api]]
+                 [org.apache.jena/jena-iri "3.10.0" :exclusions [org.slf4j/slf4j-api]]
                  
 
                  [org.mindrot/jbcrypt "0.4"]
 
-                 [org.slf4j/slf4j-log4j12 "1.7.25" :exclusions [log4j org.slf4j/slf4j-api]]
-                 [prismatic/schema "1.1.7"]
+                 [org.slf4j/slf4j-log4j12 "1.7.26" :exclusions [log4j org.slf4j/slf4j-api]]
+                 [prismatic/schema "1.1.10"]
 
-                 [ring-middleware-format "0.7.2"]
-                 [ring "1.6.2" :exclusions [org.clojure/java.classpath]]
+                 [ring-middleware-format "0.7.4"]
+                 [ring "1.7.1" :exclusions [org.clojure/java.classpath]]
                  [ring-middleware-accept "2.0.3"]
                  [ring-server "0.5.0"]
-                 [ring/ring-core "1.6.2"]
+                 [ring/ring-core "1.7.1"]
                  ;;[selmer "0.6.9"]
                  [swirrl/lib-swirrl-server "0.6.3" :exclusions [clout org.clojure/java.classpath]]
                  [wrap-verbs "0.1.1"]]
@@ -130,8 +130,8 @@
                 :dependencies [[clojure-csv/clojure-csv "2.0.2"]
                                [org.clojure/data.json "0.2.6"]
                                [ring-mock "0.1.5"]
-                               [ring/ring-devel "1.6.2" :exclusions [org.clojure/java.classpath org.clojure/tools.reader]]
-                               [eftest "0.3.2"] ;; repl test runner support
+                               [ring/ring-devel "1.7.1" :exclusions [org.clojure/java.classpath org.clojure/tools.reader]]
+                               [eftest "0.5.7"] ;; repl test runner support
                                [org.clojure/test.check "0.9.0"]]
 
                 ;;:jvm-opts ["-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005"]
