f5a2dda0929cb1ac5ec8f713cb4720d683cb940d
Andrew Mcveigh <me@andrewmcveigh.com>
2019-03-19 13:59:57 +0000

    Extract common route testing helpers

--- test/drafter/routes/user_restriction_api_test.clj
+++ test/drafter/routes/user_restriction_api_test.clj
@@ -11,64 +11,14 @@
             [schema.test :refer [validate-schemas]]
             [swirrl-server.async.jobs :refer [finished-jobs]]
             [clojure.string :as string]
-            [clojure-csv.core :as csv])
+            [clojure-csv.core :as csv]
+            [drafter.routes.common
+             :refer [live-query draftset-query append-quads-to-draftset-through-api]])
   (:import [java.io ByteArrayInputStream ByteArrayOutputStream]))
 
 (use-fixtures :each (join-fixtures [validate-schemas]))
 
 (def system-config "drafter/routes/sparql-test/graph-restriction-system.edn")
-
-(def default-sparql-query {:request-method :get
-                           :uri "/sparql/live"
-                           :query-params {"query" "SELECT * WHERE { ?s ?p ?o }"}
-                           :headers {"accept" "text/csv"}})
-
-(defn- build-query
-  ([endpoint-path query] (build-query endpoint-path query))
-  ([endpoint-path query {:keys [default-graph-uri named-graph-uri graphs]}]
-   (let [query-request (-> default-sparql-query
-                           (assoc-in [:query-params "query"] query)
-                           (assoc :uri endpoint-path))]
-
-     (-> query-request
-         (assoc-in [:query-params "default-graph-uri"] default-graph-uri)
-         (assoc-in [:query-params "named-graph-uri"] named-graph-uri)))))
-
-(defn live-query [qstr & {:as kwargs}]
-  (build-query "/v1/sparql/live" qstr kwargs))
-
-
-(defn- draftset-query [draftset qstr & {:keys [union-with-live] :as kwargs}]
-  (tc/with-identity test-editor
-    (-> (str draftset "/query")
-        (build-query qstr kwargs)
-        (cond-> union-with-live (assoc-in [:params :union-with-live] "true")))))
-
-(defn- statements->input-stream [statements format]
-  (let [bos (ByteArrayOutputStream.)
-        serialiser (rdf-writer bos :format format)]
-    (add serialiser statements)
-    (ByteArrayInputStream. (.toByteArray bos))))
-
-(defn- append-to-draftset-request [user draftset-location data-stream content-type]
-  (tc/with-identity user
-    {:uri (str draftset-location "/data")
-     :request-method :put
-     :body data-stream
-     :headers {"content-type" content-type}}))
-
-(defn- statements->append-request [user draftset-location statements format]
-  (let [input-stream (statements->input-stream statements format)]
-    (append-to-draftset-request user
-                                draftset-location
-                                input-stream
-                                (.getDefaultMIMEType (formats/->rdf-format format)))))
-
-(defn- append-quads-to-draftset-through-api [api user draftset-location quads]
-  (let [request (statements->append-request user draftset-location quads :nq)
-        response (api request)]
-    (tc/await-success finished-jobs (get-in response [:body :finished-job]))))
-
 
 (defn check-user-restrictions [run-query]
 
