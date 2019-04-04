6989411ba325f3ebb65f8f4fca2b547ca8a5e1e2
Rick Moynihan <rick.m@swirrl.com>
2019-04-04 16:49:28 +0100

    Run tests and deploy (also bump stardog)

--- travis/before_deploy.sh
+++ travis/before_deploy.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+cd `dirname $0`
+
+cd ../drafter
 mkdir -p dist
 cp target/drafter.jar dist/drafter-$TRAVIS_BRANCH-latest.jar
 cp target/drafter.jar dist/drafter-private-$TRAVIS_BRANCH-$TRAVIS_BUILD_NUMBER.jar
 mkdir -p releases
 cp target/drafter.jar releases/drafter-$TRAVIS_TAG-$TRAVIS_BUILD_NUMBER.jar
+
+cd ../drafter-client
+lein deploy
