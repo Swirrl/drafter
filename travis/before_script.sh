c45208e8384d8b631a70ce80ccf914104bc4f302
Rick Moynihan <rick.m@swirrl.com>
2019-04-08 11:43:38 +0100

    Remove before-script

--- travis/before_script.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sleep 15
-
-env JAVA_HOME=/usr/lib/jvm/java-8-oracle/bin/java /opt/stardog/stardog/$STARDOG_VERSION/bin/stardog-admin db create -n drafter-test-db -o strict.parsing=false -o query.all.graphs=true -o reasoning.type=none --
-
-sleep 10
