73fb7eb70f54b827c79683b9ca4e78140162abec
Rick Moynihan <rick.m@swirrl.com>
2019-04-10 16:11:38 +0100

    Remove deploy of drafter-client
    This should be moved into a travis releases section

--- travis/before_deploy.sh
+++ travis/before_deploy.sh
@@ -10,6 +10,3 @@ cp target/drafter.jar dist/drafter-$TRAVIS_BRANCH-latest.jar
 cp target/drafter.jar dist/drafter-private-$TRAVIS_BRANCH-$TRAVIS_BUILD_NUMBER.jar
 mkdir -p releases
 cp target/drafter.jar releases/drafter-$TRAVIS_TAG-$TRAVIS_BUILD_NUMBER.jar
-
-cd ../drafter-client
-lein deploy
