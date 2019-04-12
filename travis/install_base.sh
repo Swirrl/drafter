c6bc17cc2f5a040c4303341a42e96423a8105517
Rick Moynihan <rick.m@swirrl.com>
2019-04-12 11:25:21 +0100

    Move stardog fetching to install_base script

--- travis/install_base.sh
+++ travis/install_base.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
-apt-get update
-apt-get install -y git unzip build-essential apache2-utils wget bsdtar python-pip
-chown -R travis ./travis/*
-chmod +x ./travis/*
-mkdir -p /etc/leiningen/
-mv ./travis/profiles.clj /etc/leiningen/profiles.clj
+if [ -n "$TRAVIS" ] ; then
+    echo Setting up travis dependencies...
+    #apt-get update  ## This breaks pip/awscli
+    apt-get install -y git unzip build-essential apache2-utils wget bsdtar python-pip
+    chown -R travis ./travis/*
+    chmod +x ./travis/*
+    mkdir -p /etc/leiningen/
+    mv ./travis/profiles.clj /etc/leiningen/profiles.clj
+    export AWS_INSTALL=true ## used to parameterise curl command below...
+fi
+
+echo Fetching stardog
+curl 'https://raw.githubusercontent.com/Swirrl/stardog/master/consume/setup-stardog?token=AABCpLUdCWZQ-1MW8nm70bQLGqZJfGNeks5csMkIwA%3D%3D' | \
+    env STARDOG_BUILD=stardog-5.2.0-45 \
+        CREATE_DB=drafter-test-db \
+        START_STARDOG=true \
+        bash ### <--- bash command here runs curled script
