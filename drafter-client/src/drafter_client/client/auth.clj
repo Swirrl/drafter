f00d0853201985229fde68a5c6d3e141c677fd81
Andrew Mcveigh <me@andrewmcveigh.com>
2019-04-17 14:34:26 +0100

    Add basic auth interceptor

--- drafter-client/src/drafter_client/client/auth.clj
+++ drafter-client/src/drafter_client/client/auth.clj
@@ -18,3 +18,12 @@
             (assoc-in ctx
                       [:request :headers "Authorization"]
                       (jws-auth-header client user)))})
+
+(defn basic-auth [user password]
+  (let [bpass (.getBytes (str user \: password))]
+    {:name ::auth-header
+     :enter (fn [ctx]
+              (assoc-in ctx [:request :headers "Authorization"]
+                        (str "Basic "
+                             (-> (java.util.Base64/getEncoder)
+                                 (.encodeToString bpass)))))}))
