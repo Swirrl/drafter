f8bdb9155754e3c5d23a6a2e2d526b57835fedac
lkitching <lee@swirrl.com>
2019-05-08 15:59:06 +0100

    Move service installation into its own script.
    Add install_services.sh script for installing service dependencies.
    This should not be run as sudo since the ssh keye to add to ssh-agent
    should be the one belonging to the travis user.

--- travis/install_base.sh
+++ travis/install_base.sh
@@ -8,14 +8,3 @@ mkdir -p /etc/leiningen/
 mv ./travis/profiles.clj /etc/leiningen/profiles.clj
 
 ./travis/install_clojure.sh
-
-# start ssh agent and add key so clojure tools can clone git repos
-ssh-agent
-ssh-add
-
-# install service dependencies
-clojure -M:omni install-dependencies
-
-# start services
-./.omni_cache/install/stardog/install/dev-start.sh
-./.omni_cache/install/mongodb/install/dev-start.sh
