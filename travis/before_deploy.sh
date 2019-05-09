86169d0c1f02591771012fa4521cb161f82670b4
lkitching <lee@swirrl.com>
2019-05-09 10:15:15 +0100

    Build omni package on successful build.

--- travis/before_deploy.sh
+++ travis/before_deploy.sh
@@ -10,3 +10,6 @@ cp target/drafter.jar dist/drafter-$TRAVIS_BRANCH-latest.jar
 cp target/drafter.jar dist/drafter-private-$TRAVIS_BRANCH-$TRAVIS_BUILD_NUMBER.jar
 mkdir -p releases
 cp target/drafter.jar releases/drafter-$TRAVIS_TAG-$TRAVIS_BUILD_NUMBER.jar
+
+# setup omni package
+cp target/drafter.jar package/install/drafter-$TRAVIS_TAG-$TRAVIS_BUILD_NUMBER.jar
