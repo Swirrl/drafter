5055904e356a718cb53d7bf775ee7be4dcc41cdd
Andrew Mcveigh <me@andrewmcveigh.com>
2019-07-02 11:41:49 +0100

    Correct uberjar config so it includes prod env

--- package/install/dev-start.sh
+++ package/install/dev-start.sh
@@ -5,8 +5,7 @@ cd {{omni/install-dir}}
 set -o allexport
 source dev-settings.env
 
-java -jar ./drafter.jar &
+java -jar ./drafter.jar $@ &
 
 #TODO: wait for drafter to become available
 echo $! > drafter.pid
-
