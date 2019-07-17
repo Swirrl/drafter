f1f9afaafc156d2641a4ca38372a3ab86e825c14
Rick Moynihan <rick.m@swirrl.com>
2019-07-17 14:47:31 +0100

    Improve error reporting when env vars are not set for SPARQL endpoints
    IIRC there used to be better error reporting here, but as we've been
    slowly moving from aero to integrant it appears to have got lost.  So
    prior to this commit starting drafter without the appropriate env vars
    being set would result in a rather opaque error.
    
    This is a step towards a predominantly integrant based configuration
    mechanism.

--- drafter/src/drafter/common/config.clj
+++ drafter/src/drafter/common/config.clj
@@ -1,8 +1,25 @@
 (ns drafter.common.config
-  (:require [integrant.core :as ig]))
+  (:require [integrant.core :as ig]
+            [clojure.spec.alpha :as s]))
 
 ;; A simple component for storing arbitrary data under a key in an
 ;; integrant system map.
+
+(defmethod ig/pre-init-spec :drafter.common.config/sparql-query-endpoint [_]
+  (s/spec string?))
+
+(defmethod ig/init-key :drafter.common.config/sparql-query-endpoint [_ uri-str]
+  (java.net.URI. uri-str))
+
+(defmethod ig/pre-init-spec :drafter.common.config/sparql-update-endpoint [_]
+  (s/spec string?))
+
+(defmethod ig/init-key :drafter.common.config/sparql-update-endpoint [_ uri-str]
+  (java.net.URI. uri-str))
+
+
+;; TODO: we should probably remove this component now we've moved its
+;; properties :sparql-query-endpoint and :sparql-update-endpoint to
+;; the top-level system.
 (defmethod ig/init-key :drafter.common/config [_ opts]
   opts)
-
