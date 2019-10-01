eed86dc96e25b095a0b65d11e53866eb7ef9f6a0
Andrew Mcveigh <me@andrewmcveigh.com>
2019-10-01 11:51:38 +0100

    Fix repo/make-repo, actually using `params`

--- drafter-client/src/drafter_client/client/repo.clj
+++ drafter-client/src/drafter_client/client/repo.clj
@@ -1,18 +1,21 @@
 (ns drafter-client.client.repo
-  (:require [drafter-client.client.auth :as auth]
+  (:require [cemerick.url :as url]
             [drafter-client.client.draftset :as draftset]
             [drafter-client.client.impl :as i]
             [grafter-2.rdf4j.repository :as repo]
             [martian.core :as martian]))
 
 (defn make-repo [client context token params]
-  (let [params (merge {:query ""} params)
+  (let [params (update params :query (fnil identity ""))
         [endpoint params] (if (draftset/live? context)
                             [:get-query-live params]
                             [:get-query-draftset
-                             (assoc params :id (str (draftset/id context)))])
-        {:keys [url headers] :as req}
+                             (assoc params
+                                    :id (str (draftset/id context))
+                                    :union-with-live true)])
+        {:keys [url query-params headers] :as req}
         (-> (i/set-bearer-token client token)
-            (martian/request-for endpoint params))]
-    (doto (repo/sparql-repo url)
+            (martian/request-for endpoint params))
+        query-params (dissoc query-params :query)]
+    (doto (repo/sparql-repo (str url \? (url/map->query query-params)))
       (.setAdditionalHttpHeaders (select-keys headers ["Authorization"])))))
