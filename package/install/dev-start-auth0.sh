f6d505395274593237a1b6a789db7296451f03cc
Rick Moynihan <rick.m@swirrl.com>
2019-11-27 13:44:34 +0000

    Configure stasher to be off in the omni dev package we export
    This is because caching can often interfere with development in two
    ways:
    
    1. It can mask performance issues and make you think things are faster
       than they are.
    2. If you mutate the data behind drafter you may get
       misleading/incorrect results, resulting in wasted time
       chasing red herrings.

--- package/install/dev-start-auth0.sh
+++ package/install/dev-start-auth0.sh
@@ -5,7 +5,7 @@ cd {{omni/install-dir}}
 set -o allexport
 source dev-settings.env
 
-java -jar ./drafter.jar drafter-dev-auth0.edn &
+java -jar ./drafter.jar drafter-dev-auth0.edn stasher-off.edn &
 
 #TODO: wait for drafter to become available
 echo $! > drafter.pid
