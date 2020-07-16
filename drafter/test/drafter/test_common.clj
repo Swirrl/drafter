384f7be47a078e2fba456ca2ddb6d521ba3910be
Andrew Mcveigh <me@andrewmcveigh.com>
2020-07-16 10:19:33 +0100

    Double timeout because :shrug:

--- drafter/test/drafter/test_common.clj
+++ drafter/test/drafter/test_common.clj
@@ -280,7 +280,7 @@
                             (.replace "< " "<"))
                         "}")))
 
-(def default-timeout 10000)
+(def default-timeout 20000)
 
 (def job-id-path #"/v1/status/finished-jobs/([a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})")
 
