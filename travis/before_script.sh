420106b9f279350a32d00f2378227b459015f7a6
RicSwirrl <ric@swirrl.com>
2020-07-29 15:53:20 +0100

    refactor travis before script to use stardog.properties, and add new properites for stardog7 counts. muttnik#670

--- travis/before_script.sh
+++ travis/before_script.sh
@@ -5,17 +5,11 @@ sleep 15
 env JAVA_HOME=/usr/lib/jvm/java-8-oracle/bin/java \
     ./.omni_cache/install/stardog/install/stardog/bin/stardog-admin db create \
     -n drafter-test-db \
-    -o strict.parsing=false \
-    -o query.all.graphs=true \
-    -o reasoning.schema.graphs='http://publishmydata.com/graphs/reasoning-tbox' \
-    -o reasoning.type=SL --
+    -c ./travis/stardog.properties
 
 env JAVA_HOME=/usr/lib/jvm/java-8-oracle/bin/java \
     ./.omni_cache/install/stardog/install/stardog/bin/stardog-admin db create \
     -n drafter-client-test \
-    -o strict.parsing=false \
-    -o query.all.graphs=true \
-    -o reasoning.schema.graphs='http://publishmydata.com/graphs/reasoning-tbox' \
-    -o reasoning.type=SL --
+    -c ./travis/stardog.properties
 
 sleep 10
