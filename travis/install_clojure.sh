5cdc95f5410bdc5b19195749874767906563ddc7
Rick Moynihan <rick.m@swirrl.com>
2020-08-12 11:19:14 +0100

    Upgrade tools.deps to latest

--- travis/install_clojure.sh
+++ travis/install_clojure.sh
@@ -3,6 +3,6 @@
 set -o errexit
 
 apt-get install rlwrap
-curl -O https://download.clojure.org/install/linux-install-1.10.0.442.sh
-chmod +x linux-install-1.10.0.442.sh
-sudo ./linux-install-1.10.0.442.sh
+curl -O https://download.clojure.org/install/linux-install-1.10.1.561.sh
+chmod +x linux-install-1.10.1.561.sh
+sudo ./linux-install-1.10.1.561.sh
