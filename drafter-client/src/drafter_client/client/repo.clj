829dd9af3f73a48cb0b6b99165a9fa72e74eab1a
Rick Moynihan <rick.m@swirrl.com>
2020-10-07 12:19:23 +0100

    Add a docstring to make-repo

--- drafter-client/src/drafter_client/client/repo.clj
+++ drafter-client/src/drafter_client/client/repo.clj
@@ -6,13 +6,29 @@
             [grafter-2.rdf4j.repository :as repo]
             [martian.core :as martian]))
 
-(defn make-repo [client context token params]
+(defn make-repo
+  "Takes a DrafterClient and returns a grafter sparql-repo on it with
+  the appropriate authorization derived from either the
+  DrafterClient's :auth-provider or by using token if it is non-nil.
+
+  Args are:
+
+  client   - The DrafterClient to derive this repo from.
+
+  endpoint - Either a draftset (as returned by get-draftset(s) ) or
+             the value :drafter-client.client.draftset/live for the public
+             endpoint.
+
+  token    - An auth0 token or nil (if DrafterClient is configured with an :auth-provider)
+
+  params   - A map of extra parameters to pass to the endpoint. "
+  [client endpoint token params]
   (let [params (update params :query (fnil identity ""))
-        [endpoint params] (if (draftset/live? context)
+        [endpoint params] (if (draftset/live? endpoint)
                             [:get-query-live params]
                             [:get-query-draftset
                              (assoc params
-                                    :id (str (draftset/id context))
+                                    :id (str (draftset/id endpoint))
                                     :union-with-live true)])
         {:keys [url query-params headers]
          :as req} (-> (i/with-authorization client token)
