86186c78dfedfa5a79bbf472256428fb29734158
Rick Moynihan <rick.m@swirrl.com>
2020-10-16 10:15:32 +0100

    Improve support for legacy drafter v2.2.2

--- drafter-client/src/drafter_client/client/util.clj
+++ drafter-client/src/drafter_client/client/util.clj
@@ -3,8 +3,25 @@
             [clj-time.core :as t])
   (:import [java.util UUID]))
 
-(defn date-time [s]
-  (some->> s (parse (formatters :date-time))))
+(defn- parse-legacy-drafter-date-time
+  "Try parsing iso date-time without ms. This format was returned by
+  legacy drafters and appears to have changed between drafter 2.2.2
+  and 2.2.3 with the change over to java.time."
+  [s]
+  (some->> s (parse (formatters :date-time-no-ms))))
+
+(defn date-time
+  "Try to parse the string as an ISO date time with or without ms, e.g.
+  should accept date-times like: 2020-10-16T08:51:59.000Z and
+  2020-10-16T08:51:59Z.
+  "
+  [s]
+  (try
+    (some->> s (parse (formatters :date-time)))
+    (catch IllegalArgumentException ex
+      ;; Raised if there's a parse error. We should be able to remove
+      ;; this after we've upgraded all deployments of drafter 2.2.2
+      (parse-legacy-drafter-date-time s))))
 
 (defn now []
   (t/now))
