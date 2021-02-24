63a5a6bb186ff32f9692b19f2378d67ed868b0a2
lkitching <lee@swirrl.com>
2021-02-24 15:38:24 +0000

    Remove ordered list of operations affecting graphs from graphs-meta
    Issue #490 - Remove the :ops key in the graph-meta map which contains
    the ordered sequence of jena update operations affecting each graph.

--- drafter/src/drafter/feature/draftset/update.clj
+++ drafter/src/drafter/feature/draftset/update.clj
@@ -171,20 +171,9 @@
                          :live? false}])
         affected-graphs (->> (.getOperations update-request)
                              (map (juxt identity affected-graphs)))
-        graph-op-order (reduce (fn [acc [op gs]]
-                                 (reduce (fn [acc g]
-                                           (if-let [ops (acc g)]
-                                             (update acc g conj op)
-                                             (assoc acc g [op])))
-                                         acc
-                                         gs))
-                               {}
-                               affected-graphs)
         affected-graphs (->> (map second affected-graphs)
                              (apply set/union)
                              (map new-graph)
-                             (map (fn [[lg m]]
-                                    [lg (assoc m :ops (graph-op-order lg))]))
                              (into {}))]
     (->> (merge-with merge
                      affected-graphs
