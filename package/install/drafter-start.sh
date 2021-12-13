412f3021609819f7f433dab8f31793c8727fba08
RicSwirrl <ric@swirrl.com>
2021-12-13 15:19:10 +0000

    2.5.1

--- package/install/drafter-start.sh
+++ package/install/drafter-start.sh
@@ -8,6 +8,7 @@ DRAFTER_JVM_OPTS="-Xmx4g \
                   -Dcom.sun.management.jmxremote.port=3007 \
                   -Dhttp.maxConnections=60 \
                   -Dorg.eclipse.jetty.server.Request.maxFormContentSize=41943040 \
+                  -Dlog4j2.formatMsgNoLookups=true \
                   -Dlog4j.configurationFile=log4j2.xml"
 
 java ${DRAFTER_JVM_OPTS} -cp 'lib/*:drafter.jar' clojure.main -m drafter.main drafter-prod.edn
