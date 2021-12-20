6bc14b56fac27ac89a95789841650cb7731badf4
Rick Moynihan <rick.m@swirrl.com>
2021-12-20 14:24:24 +0000

    Use latest clojure CLI tools

--- travis/install_clojure.sh
+++ travis/install_clojure.sh
@@ -3,6 +3,7 @@
 set -o errexit
 
 apt-get install rlwrap
-curl -O https://download.clojure.org/install/linux-install-1.10.1.561.sh
-chmod +x linux-install-1.10.1.561.sh
-sudo ./linux-install-1.10.1.561.sh
+
+curl -O https://download.clojure.org/install/linux-install-1.10.3.1040.sh
+chmod +x linux-install-1.10.3.1040.sh
+sudo ./linux-install-1.10.3.1040.sh
