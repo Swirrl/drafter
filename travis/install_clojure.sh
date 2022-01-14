877195e019c4eaf4f277bc0a2ce9d217e97bad0d
Rick Moynihan <rick.m@swirrl.com>
2022-01-14 16:49:35 +0000

    Delete script to install clojure as it comes with the image

--- travis/install_clojure.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -o errexit
-
-apt-get install rlwrap
-
-curl -O https://download.clojure.org/install/linux-install-1.10.3.1040.sh
-chmod +x linux-install-1.10.3.1040.sh
-sudo ./linux-install-1.10.3.1040.sh
