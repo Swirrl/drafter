002ce7aff39ca2eccdfa8df2cd10f04f096c61a0
Rick Moynihan <rick.m@swirrl.com>
2022-01-17 11:53:24 +0000

    Change to -M to avoid using deprecated tools.deps calling convention

--- .circleci/install_services.sh
+++ .circleci/install_services.sh
@@ -7,7 +7,7 @@ set -o errexit
 # ssh-add
 
 # install service dependencies
-clojure -A:omni install-dependencies --dependencies dependencies-mongo-auth.edn
+clojure -M:omni install-dependencies --dependencies dependencies-mongo-auth.edn
 
 # start services
 ./.omni_cache/install/stardog/install/dev-start.sh
