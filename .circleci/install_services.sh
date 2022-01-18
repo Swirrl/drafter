b89e8b7d98a56461fc43957c2b5f0f8f80a480d4
Rick Moynihan <rick.m@swirrl.com>
2022-01-18 12:36:27 +0000

    Start services again

--- .circleci/install_services.sh
+++ .circleci/install_services.sh
@@ -8,16 +8,14 @@ set -o errexit
 
 # install service dependencies
 
-echo AWS access key: $AWS_ACCESS_KEY_ID
-
 clojure -M:omni install-dependencies --dependencies dependencies-mongo-auth.edn -vvv
 
 echo "Done omni install dependencies"
 
-ls -laR ./.omni_cache/
+
 
 # start services
-# ./.omni_cache/install/stardog/install/dev-start.sh
-# if [[ -d ./.omni_cache/install/mongodb ]]; then
-# #    ./.omni_cache/install/mongodb/install/dev-start.sh
-# fi
+./.omni_cache/install/stardog/install/dev-start.sh
+if [[ -d ./.omni_cache/install/mongodb ]]; then
+    ./.omni_cache/install/mongodb/install/dev-start.sh
+fi
