26c016e69e73879e25f17ae934227c527f4c0240
Callum Oakley <hello@callumoakley.net>
2022-03-11 14:43:10 +0000

    push to gcp

--- drafter/build.clj
+++ drafter/build.clj
@@ -1,8 +1,7 @@
 (ns build
   (:require [clojure.tools.build.api :as b]
             [clojure.string :as str]
-            [juxt.pack.api :as pack])
-  (:refer-clojure :exclude [test]))
+            [juxt.pack.api :as pack]))
 
 ;; A tag name must be valid ASCII and may contain lowercase and uppercase
 ;; letters, digits, underscores, periods and dashes
@@ -11,27 +10,17 @@
   (str/replace s #"[^a-zA-Z0-9_.-]" "_"))
 
 (defn pmd4-docker-build [opts]
-  (let [drafter-basis (b/create-basis {:project "deps.edn" :aliases [:pmd4/docker]})]
-    (-> opts
-        (assoc :basis drafter-basis
-               :image-name "swirrl/drafter-pmd4" ; TODO include the destination registry details?
-               :image-type (or (opts :image-type) :docker)
-               :include {"/app/config" ["./resources/drafter-auth0.edn"]}
-               :base-image "gcr.io/distroless/java:11"
-               :volumes #{"/app/config" "/app/stasher-cache"}
-               :to-registry-username (System/getenv "DOCKERHUB_USERNAME")
-               :to-registry-password (System/getenv "DOCKERHUB_PASSWORD")
-               :tags (into #{}
-                           (map #(tag (b/git-process {:git-args %})))
-                           ["describe --tags"
-                            "rev-parse HEAD"
-                            "branch --show-current"]))
-        (pack/docker))))
-
-(comment (pmd4-docker-build nil))
-
-(defn build [opts]
-  (-> opts
-      (pmd4-docker-build)))
-
-(comment (build nil))
+  (pack/docker
+   {:basis (b/create-basis {:project "deps.edn" :aliases [:pmd4/docker]})
+    :image-name "europe-west2-docker.pkg.dev/swirrl-devops-infrastructure-1/swirrl/drafter-pmd4"
+    :image-type (get opts :image-type :docker)
+    :include {"/app/config" ["./resources/drafter-auth0.edn"]}
+    :base-image "gcr.io/distroless/java:11"
+    :volumes #{"/app/config" "/app/stasher-cache"}
+    :to-registry-username "_json_key"
+    :to-registry-password (System/getenv "GCLOUD_SERVICE_KEY")
+    :tags (into #{}
+                (map #(tag (b/git-process {:git-args %})))
+                ["describe --tags"
+                 "rev-parse HEAD"
+                 "branch --show-current"])}))
