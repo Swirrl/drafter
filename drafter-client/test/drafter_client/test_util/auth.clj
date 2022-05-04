b6397ada4c211055e293161ee92cf0a0f645bacc
Callum Oakley <hello@callumoakley.net>
2022-05-04 15:00:32 +0100

    permissions in client tests

--- drafter-client/test/drafter_client/test_util/auth.clj
+++ drafter-client/test/drafter_client/test_util/auth.clj
@@ -1,19 +1,22 @@
 (ns drafter-client.test-util.auth
-  (:require [drafter-client.test-util.jwt :as jwt]
+  (:require [drafter.test-common :refer [token]]
+            [drafter.user :refer [role->permissions]]
             [environ.core :refer [env]]))
 
 (def system-user {:email "system@swirrl.com" :role "system"})
 
 (defn system-token []
-  (jwt/token (env :auth0-domain)
-             (env :auth0-aud)
-             "system@swirrl.com"
-             "drafter:system"))
+  (token (env :auth0-domain)
+         (env :auth0-aud)
+         "system@swirrl.com"
+         "drafter:system"
+         (role->permissions :system)))
 
 (def test-publisher {:email "publisher@swirrl.com" :role "publisher"})
 
 (defn publisher-token []
-  (jwt/token (env :auth0-domain)
-             (env :auth0-aud)
-             "publisher@swirrl.com"
-             "drafter:publisher"))
+  (token (env :auth0-domain)
+         (env :auth0-aud)
+         "publisher@swirrl.com"
+         "drafter:publisher"
+         (role->permissions :publisher)))
