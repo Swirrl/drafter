395fab89b87f239123e52743e2f7544c0a598c43
lkitching <lee@swirrl.com>
2022-07-08 18:01:23 +0100

    Add tools.build tasks to build jar, pack and uberjar
    Issue #635 - Add 'jar' and 'install' tasks to build.clj. The jar
    tasks builds a jar for the current drafter version and the install
    tasks builds and installs the jar into the local maven repository.
    
    Add an 'uber' task for building an uberjar for the current version.
    
    Add a 'clean' task to clean the target directory.
    
    Add 'skinny' task which builds a skinny jar with pack. Add a :skinny
    alias into deps.edn to include the required resources. Update the
    pack script to use this task instead of calling pack directly.
    
    The jar, install and uber tasks all support a :type option
    indicating which drafter version to build for. This should name
    an alias within deps.edn containing the production configuration
    to use. This defaults to :pmd4 if not specified.
    
    Exclude servlet-api from lib-noir dependency since this is older
    than the one required by jetty. Add top-level dependency for
    jackson-core.

--- drafter/build.clj
+++ drafter/build.clj
@@ -3,6 +3,12 @@
             [clojure.string :as str]
             [juxt.pack.api :as pack]))
 
+(def lib 'com.swirrl/drafter)
+(def version (format "2.0.%s" (b/git-count-revs nil)))
+(def class-dir "target/classes")
+(def jar-file (format "target/drafter-%s.jar" version))
+(def uberjar-file (format "target/drafter-%s-standalone.jar" version))
+
 ;; A tag name must be valid ASCII and may contain lowercase and uppercase
 ;; letters, digits, underscores, periods and dashes
 ;; https://docs.docker.com/engine/reference/commandline/tag/#extended-description
@@ -10,6 +16,12 @@
   (when s (str/replace s #"[^a-zA-Z0-9_.-]" "_")))
 
 (def repo "europe-west2-docker.pkg.dev/swirrl-devops-infrastructure-1/swirrl")
+
+(defn- type-aliases
+  "Returns the set of aliases which defines the build type in the options. If specified, the type
+   should be either :pmd3 or :pmd4. The default is :pmd4."
+  [opts]
+  #{(:type opts :pmd4)})
 
 (defn pmd4-docker-build [opts]
   (let [tags (->> ["rev-parse HEAD"
@@ -35,3 +47,68 @@
       ;; https://github.com/juxt/pack.alpha/issues/101
       :to-registry-username "_json_key"
       :to-registry-password (System/getenv "GCLOUD_SERVICE_KEY")})))
+
+(defn clean [_]
+  (b/delete {:path "target"}))
+
+(defn- jar-basis [opts]
+  (b/create-basis {:project "deps.edn" :aliases (type-aliases opts)}))
+
+(defn skinny
+  "Builds a pack 'skinny' jar and a lib directory containing all dependencies"
+  [_opts]
+  (let [basis (b/create-basis {:project "deps.edn" :aliases [:skinny]})]
+    (pack/skinny {:basis basis
+                  :path "target/drafter.jar"
+                  :path-coerce :jar
+                  :libs "target/lib"})))
+
+(defn- copy-files [basis aliases]
+  (let [alias-extra-paths (mapcat (fn [alias]
+                                    (get-in basis [:aliases alias :extra-paths]))
+                                  aliases)]
+    (b/copy-dir {:src-dirs (concat ["src" "resources"] alias-extra-paths)
+                 :target-dir class-dir})))
+
+(defn jar
+  "Builds a jar in the target directory"
+  [opts]
+  (clean opts)
+  (let [basis (jar-basis opts)]
+    (copy-files basis (type-aliases opts))
+
+    (b/write-pom {:class-dir class-dir
+                  :lib lib
+                  :version version
+                  :basis basis
+                  :src-dirs ["src"]
+                  :resource-dirs ["resources" "prod/clj/drafter"]})
+
+    (b/jar {:class-dir class-dir
+            :jar-file jar-file})))
+
+(defn install
+  "Builds a jar and publishes it to the local maven repository"
+  [opts]
+  (jar opts)
+  (let [basis (jar-basis opts)]
+    (b/install {:basis basis
+                :lib lib
+                :version version
+                :jar-file jar-file
+                :class-dir class-dir})))
+
+(defn uber
+  "Builds an uberjar in the target directory"
+  [opts]
+  (clean opts)
+
+  (let [basis (jar-basis opts)]
+    (copy-files basis (type-aliases opts))
+    (b/compile-clj {:basis basis
+                    :src-dirs ["src"]
+                    :class-dir class-dir})
+    (b/uber {:basis basis
+             :class-dir class-dir
+             :uber-file uberjar-file
+             :main 'drafter.main})))
